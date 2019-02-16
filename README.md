3b8c5e239910af995ad49ff5e3382f9d1655c7c8
joaodacolsoares <joao.dacol.soares@gmail.com>
2019-02-16 14:02:36 -0200

    Created the vue modal component

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# vue-modal-tutorial
+# modal-tutorial
 How to create a simple modal in Vuejs
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn run serve
+```
---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+<style lang="stylus">
+body {
+  background: #cacaca;
+  margin: 0;
+}
+</style>
